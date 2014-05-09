8fdba4f12200ed452ff5ceaef75781440da69bc9
xiejg11 <xiejg11@talent.gd.ctc.com>
2014-05-09 21:18:12 +0800

    Merge origin/master
    Conflicts:
    	src/main/java/org/peasant/util/BasicFileRepository.java

--- src/main/java/org/peasant/util/web/CharacterEncodingFilter.java
+++ src/main/java/org/peasant/util/web/CharacterEncodingFilter.java
@@ -6,15 +6,12 @@
 package org.peasant.util.web;
 
 import java.io.IOException;
-import java.util.Map;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  *
